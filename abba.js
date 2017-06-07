c125f31d44ef9e088eedfbb214547ff510c840e9
pshen <patrick.lc.shen@gmail.com>
2017-06-07 13:25:08 -0700

    initial solution

--- abba.js
+++ abba.js
@@ -5,10 +5,26 @@
 var patternMatch = function(pattern, string) {
   // declare storage
   var storage = {};
+  // declare current string
+  var word = '';
   // iterate through each character in pattern
-  // if character in pattern is not in storage, add to storage with string @ index
-  // if it is in storage
-    // check if string @ index is the same as storage[character]
-    // if it is, recurse
-    // if it is not, add string @ index to storage[character] string
+  for (var i = 0; i < string.length; i++) {
+    word += string[i];
+    // if character in pattern is not in storage
+    if (!storage[pattern[0]]) {
+      //add to storage with string @ index
+      storage[pattern[0]] = word;
+    } else if (storage[pattern[i]] === string) {
+      // if it is in storage
+      // check if string @ index is the same as storage[character]
+      // if it is, recurse
+      var result = patternMatch(pattern.slice(1), string.slice(i + 1));
+      if (result) {
+        return true;
+      } else {
+        delete storage[pattern[0]];
+      }
+    }
+  }
+  return false;
 };
